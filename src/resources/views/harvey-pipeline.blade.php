b7a0b4e094dd0f4b58fdd33876cc96928d54365b
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-02-14 23:03:56 -0700

    fix: stylesheet and dependencies

--- src/resources/views/harvey-pipeline.blade.php
+++ src/resources/views/harvey-pipeline.blade.php
@@ -1,23 +1,20 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <title>Project Logs</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="{{ asset('pics/favicon.ico') }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <style>
-        h1, p {color: #fff;}
-        body {background-color: #212529;}
-        button {margin: 20px 0px;}
-    </style>
 </head>
 
 <body>
     <div class="container">
         <h1>Deployment</h1>
-        <a href="harvey"><button class="btn btn-primary">Back to Dashboard</button></a>
-        @php $status_color = ( $pipeline['status'] == 'Success' ) ? 'text-success' : (( $pipeline['status'] == 'In-Progress' ) ? 'text-info' : 'text-danger'); @endphp
+        <a href="/"><button class="btn btn-primary">Back to Dashboard</button></a>
+        @php $status_color = ( $pipeline['status'] == 'Success' ) ? 'text-success' : (( $pipeline['status'] ==
+        'In-Progress' ) ? 'text-info' : 'text-danger'); @endphp
         <p><b>Project:</b> {{ $pipeline['project'] }}</p>
         <p><b>Commit:</b> {{ $pipeline['commit'] }}</p>
         <p><b>Timestamp:</b> {{ $pipeline['timestamp'] }}</p>
@@ -27,4 +24,5 @@
 
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
+
 </html>
--- src/resources/views/harvey-project.blade.php
+++ src/resources/views/harvey-project.blade.php
@@ -7,28 +7,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="{{ asset('pics/favicon.ico') }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <style>
-        h1,
-        h2,
-        p {
-            color: #fff;
-        }
-
-        body {
-            background-color: #212529;
-        }
-
-        button {
-            margin: 20px 0px;
-        }
-    </style>
 </head>
 
 <body>
     <div class="container">
         <h1>{{ $project }}</h1>
         <p><b>Locked:</b> {{ !empty($locked) ? var_export($locked) : 'false' }}</p>
-        <a href="harvey"><button class="btn btn-primary">Back to Dashboard</button></a>
+        <a href="/"><button class="btn btn-primary">Back to Dashboard</button></a>
 
         <button class="btn btn-warning" disabled>Redeploy</button>
         <button class="btn btn-secondary" disabled>{{ $locked == false ? 'Lock' : 'Unlock' }} Deployments</button>
@@ -45,9 +30,11 @@
                 </thead>
                 <tbody>
                     @foreach ($pipelines as $pipeline)
-                    @php $status_color = ( $pipeline['status'] == 'Success' ) ? 'text-success' : (( $pipeline['status'] == 'In-Progress' ) ? 'text-info' : 'text-danger'); @endphp
+                    @php $status_color = ( $pipeline['status'] == 'Success' ) ? 'text-success' : (( $pipeline['status']
+                    == 'In-Progress' ) ? 'text-info' : 'text-danger'); @endphp
                     <tr>
-                        <td><a href="harvey-pipeline?pipeline={{ $pipeline['project'] }}-{{ $pipeline['commit'] }}">{{ $pipeline['commit'] }}</a></td>
+                        <td><a href="harvey-pipeline?pipeline={{ $pipeline['project'] }}-{{ $pipeline['commit'] }}">{{
+                                $pipeline['commit'] }}</a></td>
                         <td>{{ $pipeline['timestamp'] }}</td>
                         <td class="{{ $status_color }}">{{ $pipeline['status'] }}</td>
                     </tr>
