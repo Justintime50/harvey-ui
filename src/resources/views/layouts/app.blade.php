546ac81e9cad3a816af29467f7f50e58e0930f81
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2022-03-15 00:29:17 -0600

    chore: overhaul frontend dependencies

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -7,7 +7,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="shortcut icon" href="{{ asset('pics/favicon.ico') }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <script src="https://kit.fontawesome.com/0dd4ecd465.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
@@ -16,6 +15,7 @@
     @yield('content')
 
     <script src="{{ asset('js/app.js') }}"></script>
+    <script src="https://kit.fontawesome.com/0dd4ecd465.js" crossorigin="anonymous"></script>
 </body>
 
 </html>
