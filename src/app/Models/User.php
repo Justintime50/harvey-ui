72fdb3a610a31f7307de0f15ab3c46eb287463c9
Justintime50 <39606064+Justintime50@users.noreply.github.com>
2025-03-26 16:53:00 -0600

    chore: laravel 12 upgrade

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -4,29 +4,17 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
     use HasFactory;
-    use Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
